00078893860c8a64823791764ca9e691351c7a39
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2022-07-15 15:01:50 +0930

    chore: make name generic

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,10 @@ import { logger } from './common/logger.js';
 import { createDiscordClient } from './common/discord-client.js';
 import { environment, botToken } from './common/config.js';
 import { Intents } from 'discord.js';
-
-const NAME = 'jive';
+import { name } from '../package.json';
 
 const main = async () => {
-  logger.info('Starting "%s" in "%s" mode.', NAME, environment);
+  logger.info('Starting "%s" in "%s" mode.', name, environment);
 
   // Check we have everything we need to start
   if (!botToken) throw Error(`"BOT_TOKEN" environment variable missing.`);
@@ -17,7 +16,7 @@ const main = async () => {
   await importFiles(`${dirnameImporter(import.meta.url)}/{events,commands,api}/**/*.{ts,js}`);
 
   // Create the discord.js client
-  const client = createDiscordClient(NAME, { intents: [Intents.FLAGS.GUILD_MEMBERS], partials: [], prefix: '$jive' });
+  const client = createDiscordClient(name, { intents: [Intents.FLAGS.GUILD_MEMBERS], partials: [], prefix: `$${name}` });
 
   // Connect to the discord gateway
   await client.login(botToken);
