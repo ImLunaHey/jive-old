e24bc7b3a141f895720a2837c16590e0446607d5
Luna <105922175+ImLunaHey@users.noreply.github.com>
2022-08-01 12:02:01 +0930

    feat: pin messages in threads

---
+++ src/events/pin.ts
@@ -0,0 +1,61 @@
+import { Discord, On } from 'discordx';
+import type { ArgsOf } from 'discordx';
+import { logger } from '../common/logger';
+
+const threadOwners = {
+    '979291390162894918': '802440261543264288',
+    '1003029764128374904': '699464544078266388'
+};
+
+@Discord()
+export class Pin {
+    @On('messageReactionAdd')
+    async messageReactionAdd([{ message, emoji }, user]: ArgsOf<'messageReactionAdd'>): Promise<void> {
+        // Must be in a guild
+        if (!message.guild?.id) return;
+
+        // Must be in a thread
+        if (!message.channel || !message.channel.isThread()) return;
+
+        // Must be a pin reaction
+        if (emoji.name !== ':pushpin:') return;
+
+        // This must be the owner of the thread or someone who has permission to pin messages
+        if (threadOwners[message.channel.id as keyof typeof threadOwners] !== user.id) return;
+        if (!message.guild?.members.resolve(user.id)?.permissions.has('MANAGE_MESSAGES')) return;
+
+        // Pin message
+        try {
+            await message.pin();
+        } catch (error: unknown) {
+            if (!(error instanceof Error)) throw new Error('Unknown Error ' + error);
+            logger.error('Failed to pin %s for %s in %s', message.id, user.id, message.channel.id);
+            logger.error(error.message);
+        }
+    }
+
+    @On('messageReactionRemove')
+    async messageReactionRemove([{ message, emoji }, user]: ArgsOf<'messageReactionRemove'>): Promise<void> {
+        // Must be in a guild
+        if (!message.guild?.id) return;
+
+        // Must be in a thread
+        if (!message.channel || !message.channel.isThread()) return;
+
+        // Must be a pin reaction
+        if (emoji.name !== ':pushpin:') return;
+
+        // This must be the owner of the thread or someone who has permission to pin messages
+        if (threadOwners[message.channel.id as keyof typeof threadOwners] !== user.id) return;
+        if (!message.guild?.members.resolve(user.id)?.permissions.has('MANAGE_MESSAGES')) return;
+
+        // Unpin message
+        try {
+            await message.unpin();
+        } catch (error: unknown) {
+            if (!(error instanceof Error)) throw new Error('Unknown Error ' + error);
+            logger.error('Failed to unpin %s for %s in %s', message.id, user.id, message.channel.id);
+            logger.error(error.message);
+        }
+    }
+}
