b65d0edc3206fedc793db11ee683245f07d0d70e
Luna <105922175+ImLunaHey@users.noreply.github.com>
2022-08-02 23:26:10 +0930

    fix: add reply to command

--- src/commands/report.ts
+++ src/commands/report.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from 'crypto';
 import { GuildMember, CommandInteraction, MessageEmbed } from 'discord.js';
 import { Discord, Slash, SlashOption } from 'discordx';
 import { logger } from '../common/logger.js';
@@ -19,16 +20,32 @@ export class ReportCommands {
             ephemeral: true,
         });
 
-        // Send report to mods
+        
         try {
+            // Create report ID
+            const reportId = randomUUID();
+
+            // Send report to mods
             await reportsChannel.send({
                 content: '@here a user has reported an issue',
                 embeds: [new MessageEmbed({
+                    thumbnail: {
+                        url: interaction.user.avatarURL() ?? interaction.user.defaultAvatarURL
+                    },
                     title: 'User report',
                     description: `${interaction.user} is reporting ${reportedMember} for: \n${'```'}${reason}${'```'}`,
-                    color: 'RED'
+                    color: 'RED',
+                    fields: [{
+                        name: 'Report ID',
+                        value: reportId
+                    }]
                 })]
             });
+
+            await interaction.reply({
+                content: `Thank you for your report, your report ID is ${reportId}`,
+                ephemeral: true,
+            })
         } catch (error: unknown) {
             if (!(error instanceof Error)) throw new Error('Unknown Error: ' + error);
             logger.error('Failed to send report', error);
